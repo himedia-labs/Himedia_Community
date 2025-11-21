9f46bef666ee8441000a272abc5edd0ead1bea1e
dijeungi <dijeungi@naver.com>
2025-11-21 19:13:09 +0900

    chore: common, config - import 추가 후 적용

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { setupCors } from './config/cors/cors';
+import { setupSwagger } from './config/swagger/setupSwagger';
+import { setupFilters } from './common/filter/HttpException';
+import { setupValidation } from './common/validation/setupValidation';
+import { setupInterceptors } from './common/interceptors/LoggingInterceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  setupCors(app);
+  setupFilters(app);
+  setupValidation(app);
+  setupInterceptors(app);
+
+  setupSwagger(app);
+  await app.listen(Number(process.env.PORT));
 }
-bootstrap();
+void bootstrap();
