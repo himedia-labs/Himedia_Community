03862a57b446bb17d6232c19dc48f085972957fd
dijeungi <dijeungi@naver.com>
2025-11-28 18:58:15 +0900

    chore: 미사용 파일 삭제

--- backend/src/auth/interfaces/bcrypt.interface.ts
+++
@@ -1,9 +0,0 @@
-export type HashFunction = (
-  data: string | Buffer,
-  saltOrRounds: string | number,
-) => Promise<string>;
-
-export type CompareFunction = (
-  data: string | Buffer,
-  encrypted: string,
-) => Promise<boolean>;
--- backend/src/auth/interfaces/jwt.strategy.interface.ts
+++
@@ -1,8 +0,0 @@
-import type { Request } from 'express';
-
-export type JwtFromRequestFunction = (req: Request) => string | null;
-
-export type PassportJwtStrategy = new (...args: any[]) => {
-  name: string;
-  authenticate: (...args: any[]) => void;
-};
