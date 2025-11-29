02a3c153e745c674e0d7b73b25382aee89cbfbba
dijeungi <dijeungi@naver.com>
2025-11-29 22:21:54 +0900

    chore: import 추가 및 적용

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService, ConfigType } from '@nestjs/config';
+
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
-import { setupCors } from './config/cors/cors';
-import { setupSwagger } from './config/swagger/swagger';
+import appConfig from './common/config/app.config';
+import { setupCors } from './common/cors/cors';
+import { setupSwagger } from './common/swagger/swagger';
 import { setupFilters } from './common/exception/httpException';
 import { setupValidation } from './common/validation/validation';
 import { setupInterceptors } from './common/logging/logging';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const config: ConfigType<typeof appConfig> = configService.get('app')!;
 
+  app.use(cookieParser());
   setupCors(app);
   setupFilters(app);
   setupValidation(app);
   setupInterceptors(app);
 
   setupSwagger(app);
-  await app.listen(Number(process.env.PORT));
+  await app.listen(config.port);
 }
 void bootstrap();
