84e913961070e695e80db01f289d8d113892b9e7
dijeungi <dijeungi@naver.com>
2025-11-29 17:44:38 +0900

    chore: rateLimit 제거

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { EmailModule } from './email/email.module';
 import { AuthModule } from './auth/auth.module';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV ?? 'development'}`,
+      load: [appConfig],
     }),
     DatabaseModule,
     EmailModule,
