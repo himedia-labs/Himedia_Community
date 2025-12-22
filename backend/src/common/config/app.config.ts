43ebe9d221482247e3f21d90300785d9a7ab7a97
dijeungi <dijeungi@naver.com>
2025-12-22 15:31:26 +0900

    chore: RefreshToken Hash Secret 추가

--- backend/src/common/config/app.config.ts
+++ backend/src/common/config/app.config.ts
@@ -5,12 +5,12 @@ import { registerAs } from '@nestjs/config';
  * @description 환경변수 접근을 위한 설정
  */
 export default registerAs('app', () => ({
-  port: parseInt(process.env.PORT!),
+  port: parseInt(process.env.NODE_PORT!),
   env: process.env.NODE_ENV,
 
   jwt: {
     secret: process.env.JWT_SECRET!,
-    refreshTokenHashSecret: process.env.REFRESH_TOKEN_HASH_SECRET ?? process.env.JWT_SECRET!,
+    refreshTokenHashSecret: process.env.REFRESH_TOKEN_HASH_SECRET!,
     accessExpiresIn: parseInt(process.env.ACCESS_TOKEN_EXPIRES_IN!),
     refreshExpiresInSeconds: parseInt(process.env.REFRESH_TOKEN_EXPIRES_IN_SECONDS!),
   },
