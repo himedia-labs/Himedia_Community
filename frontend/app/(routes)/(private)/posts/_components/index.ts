98198dccc02f1735a49425d118d3d452aedbe9c5
dijeungi <dijeungi@naver.com>
2026-03-11 02:10:58 +0900

    chore: private posts 컴포넌트 export 주석 정리

--- frontend/app/(routes)/(private)/posts/_components/index.ts
+++ frontend/app/(routes)/(private)/posts/_components/index.ts
@@ -1,2 +1,3 @@
+// features
 export { default as PostDetailsForm } from '@/app/(routes)/(private)/posts/_components/PostDetailsForm';
 export { default as PostPreview } from '@/app/(routes)/(private)/posts/_components/PostPreview';
